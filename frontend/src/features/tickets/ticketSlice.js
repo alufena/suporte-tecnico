210963842116cc125f15e86cb36e82a6fc8270f0
Anderson <andersonpolv@gmail.com>
2025-04-30 01:14:43 -0300

    close ticket functionality

--- frontend/src/features/tickets/ticketSlice.js
+++ frontend/src/features/tickets/ticketSlice.js
@@ -68,6 +68,24 @@ export const getTicket = createAsyncThunk(
     }
 );
 
+export const closeTicket = createAsyncThunk(
+    'tickets/close',
+    async (ticketId, thunkAPI) => {
+        try {
+            const token = thunkAPI.getState().auth.user.token;
+            return await ticketService.closeTicket(ticketId, token);
+        } catch (error) {
+            const message =
+                (error.response &&
+                    error.response.data &&
+                    error.response.data.message) ||
+                error.message ||
+                error.toString();
+            return thunkAPI.rejectWithValue(message);
+        }
+    }
+);
+
 export const ticketSlice = createSlice({
     name: 'ticket',
     initialState,
@@ -114,6 +132,14 @@ export const ticketSlice = createSlice({
                 state.isLoading = false;
                 state.isError = true;
                 state.message = action.payload;
+            })
+            .addCase(closeTicket.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.tickets.map((ticket) =>
+                    ticket._id === action.payload._id
+                        ? (ticket.status = 'fechado')
+                        : ticket
+                ); // "tickets" é o array que será iterado por map e por lá definido o ticket correto através do id vindo do payload (action). dessa forma se consegue por o status como "fechado". aqui só faz efeito na UI; reflete na tela sem precisar recarregar a página
             });
     },
 });
--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -1,9 +1,9 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { getTicket, reset } from '../features/tickets/ticketSlice';
+import { getTicket, reset, closeTicket } from '../features/tickets/ticketSlice';
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
 function Ticket() {
@@ -11,6 +11,7 @@ function Ticket() {
     (state) => state.tickets
   );
   const params = useParams();
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const { ticketId } = useParams(); // pega o ticket da url
   useEffect(() => {
@@ -20,6 +21,12 @@ function Ticket() {
     dispatch(getTicket(ticketId));
     // eslint-disable-next-line
   }, [isError, message, ticketId]);
+  const onTicketClose = () => {
+    // event handler de fechar um ticket
+    dispatch(closeTicket(ticketId));
+    toast.success('Ticket fechado');
+    navigate('/tickets');
+  };
   if (isLoading) {
     return <Spinner />;
   }
@@ -30,21 +37,31 @@ function Ticket() {
     <div className="ticket-page">
       <header className="ticket-header">
         <BackButton url="/tickets" />
-        <h2>
-          ID do ticket {ticket._id}
+        <h2 style={{ marginBottom: '0px' }}>
+          ID do ticket
           <span className={`status status-${ticket.status}`}>
             {ticket.status}
           </span>
         </h2>
-        <h3>
-          Data enviada {new Date(ticket.createdAt).toLocaleString('pt-br')}
-        </h3>
+        <p style={{ marginTop: '0px' }}>{ticket._id}</p>
+        <h3 style={{ marginBottom: '0px' }}>Data enviada</h3>
+        <p style={{ marginTop: '0px' }}>
+          {new Date(ticket.createdAt).toLocaleString('pt-br')}
+        </p>
+        <h3 style={{ marginBottom: '0px' }}>Categoria</h3>
+        <p style={{ marginTop: '0px' }}>{ticket.product}</p>
         <hr />
         <div className="ticket-desc">
           <h3>Descrição do problema </h3>
           <p>{ticket.description}</p>
         </div>
       </header>
+      {ticket.status !== 'fechado' && (
+        <button className="btn btn-block btn-danger" onClick={onTicketClose}>
+          Encerrar ticket
+        </button>
+      )}
+      {/* só exibe o botão caso o ticket não esteja fechado */}
     </div>
   );
 }
